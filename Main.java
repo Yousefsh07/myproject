94a1345edcf168e5b75914825edc203245e7cdf9
Yousef <yousefsheikho07@gmail.com>
2023-09-25 18:11:35 +0200

    this is an update

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello this is new branch!");
+        System.out.println("this a new update");
     }
 }
